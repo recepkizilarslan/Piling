af7ee5e3cc7d76aafcc29e8f4eb80e9620f399a9
Recep Kızılarslan <72816340+recepkizilarslan@users.noreply.github.com>
2021-11-08 22:31:42 +0300

    simple custom exception mechanism added

---
+++ Piling/PilingException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace Piling
+{
+    /// <summary>
+    /// Simple custom exception class
+    /// </summary>
+    public class PilingException:Exception
+    {
+        /// <summary>
+        /// Default constructor with param
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="shutdown"></param>
+        public PilingException(string message,bool shutdown)
+        {
+              Console.WriteLine(message, Color.Red);
+
+              if (!shutdown) return;
+
+              Console.WriteLine("Please restart the application");
+              Console.ReadKey();
+              Environment.Exit(0);
+        }
+    }
+}
